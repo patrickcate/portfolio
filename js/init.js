5872004c0ab34bfbab039fc59eff499ffc86f7a7
patrickcate <git@patrickcate.com>
2015-01-08 00:01:49 -0500

    Added srcset generation and settings.

--- js/init.js
+++ js/init.js
@@ -16,6 +16,21 @@
 				enableEscapeKey: true,
 				modal: false,
 				preload: [1,3],
+				retina:
+				{
+				    ratio: 1.5, // Increase this number to enable retina image support.
+				    // Image in popup will be scaled down by this number.
+				    // Option can also be a function which should return a number (in case you support multiple ratios). For example:
+				    // ratio: function() { return window.devicePixelRatio === 1.5 ? 1.5 : 2  }
+
+
+				    replaceSrc: function(item, ratio) {
+				    	// return item.src.replace(/\.\w+$/, function(m) { return '@2x' + m; });
+				    	var regex = /\-\-\w+/;
+				    	var hdpi = '--huge';
+				    	return item.src.replace(regex, hdpi);
+				    } // function that changes image source
+				},
 				gallery:
 				{
 					enabled: true
@@ -41,29 +56,29 @@
 					}
 				}
 			});
-		});
+});
 
-		try
+try
+{
+	$('#js-contact-form').parsley(
+	{
+		errorsMessagesDisabled: true,
+		successClass: 'form__element--is-valid',
+		errorClass: 'form__element--is-invalid',
+		errorsWrapper: '',
+		errorTemplate: '',
+	});
+
+	$('#js-contact-form').submit(function()
+	{
+		$('#js-form__button').text('Sending...');
+
+		$.ajax({
+			dataType: 'jsonp',
+			url: "http://getsimpleform.com/messages/ajax?form_api_token=a325ecabd25612a592081a155d6386bd",
+			data: $('#js-contact-form').serialize()
+		}).done(function()
 		{
-			$('#js-contact-form').parsley(
-			{
-				errorsMessagesDisabled: true,
-				successClass: 'form__element--is-valid',
-				errorClass: 'form__element--is-invalid',
-				errorsWrapper: '',
-				errorTemplate: '',
-			});
-
-			$('#js-contact-form').submit(function()
-			{
-				$('#js-form__button').text('Sending...');
-
-				$.ajax({
-					dataType: 'jsonp',
-					url: "http://getsimpleform.com/messages/ajax?form_api_token=a325ecabd25612a592081a155d6386bd",
-					data: $('#js-contact-form').serialize()
-				}).done(function()
-				{
 					// Callback which can be used to show a thank you message and reset the form
 					// alert('Thank you, for contacting us');
 					$('#js-form__button').addClass('icon--checkmark button--success').removeClass('icon--send').text('Message Sent!');
@@ -73,11 +88,11 @@
 				// Stop the form from submitting
 				return false;
 			});
-		}
-		catch(err)
-		{
+}
+catch(err)
+{
 
-		}
+}
 
 		// Add HTML class when viewed as a webapp.
 		if (window.navigator.standalone === true)
